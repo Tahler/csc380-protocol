65fc335ffd44874265828bd57c09c406c56ebd83
Tyler Berry <tyler@berryac.com>
2016-10-11 21:02:39 -0600

    client-side repo implementation (proxy)

---
+++ src/edu/neumont/csc380/clientserver/client/RemoteRepository.java
@@ -0,0 +1,139 @@
+package edu.neumont.csc380.clientserver.client;
+
+import edu.neumont.csc380.clientserver.models.repo.Repository;
+import edu.neumont.csc380.clientserver.models.repo.RepositoryFullException;
+import edu.neumont.csc380.clientserver.protocol.request.*;
+import edu.neumont.csc380.clientserver.protocol.response.ContainsKeySuccessResponse;
+import edu.neumont.csc380.clientserver.protocol.response.GetSuccessResponse;
+import edu.neumont.csc380.clientserver.protocol.response.LockSuccessResponse;
+import edu.neumont.csc380.clientserver.protocol.response.Response;
+import edu.neumont.csc380.clientserver.protocol.serialization.RequestWriter;
+import edu.neumont.csc380.clientserver.protocol.serialization.ResponseReader;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class RemoteRepository implements Repository<String, Object> {
+    private static final int MAX_WAIT_TIME = 500;
+
+    private final String host;
+    private final int port;
+
+    public RemoteRepository(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    @Override
+    public boolean containsKey(String key) {
+        Response response = this.makeRequest(new ContainsKeyRequest(key));
+
+        boolean containsKey;
+        if (response.getType() == Response.Type.CONTAINS_KEY_SUCCESS) {
+            ContainsKeySuccessResponse containsKeySuccessResponse = (ContainsKeySuccessResponse) response;
+            containsKey = containsKeySuccessResponse.containsKey();
+        } else {
+            throw new RuntimeException("Server returned bad response: " + response.getType());
+        }
+        return containsKey;
+    }
+
+    @Override
+    public void put(String key, Object value) {
+        Response response = this.makeRequest(new PutRequest(key, value));
+
+        Response.Type responseType = response.getType();
+
+        if (responseType != Response.Type.PUT_SUCCESS) {
+            if (responseType == Response.Type.SERVER_FULL) {
+                throw new RepositoryFullException();
+            } else {
+                throw new RuntimeException("Server returned bad response: " + response.getType());
+            }
+        }
+    }
+
+
+    @Override
+    public Object get(String key) {
+        Response response = this.makeRequestUntilNotLocked(new GetRequest(key));
+
+        Object value;
+        if (response.getType() == Response.Type.GET_SUCCESS) {
+            GetSuccessResponse responseWithValue = (GetSuccessResponse) response;
+
+            value = responseWithValue.getValue();
+        } else {
+            throw new RuntimeException("Server returned bad response: " + response.getType());
+        }
+        return value;
+    }
+
+    @Override
+    public Object lock(String key) {
+        Response response = this.makeRequestUntilNotLocked(new LockRequest(key));
+
+        Object value;
+        if (response.getType() == Response.Type.LOCK_SUCCESS) {
+            LockSuccessResponse responseWithValue = (LockSuccessResponse) response;
+
+            value = responseWithValue.getValue();
+        } else {
+            throw new RuntimeException("Server returned bad response: " + response.getType());
+        }
+        return value;
+    }
+
+    @Override
+    public void update(String key, Object value) {
+        Response response = this.makeRequestUntilNotLocked(new UpdateRequest(key, value));
+
+        if (response.getType() != Response.Type.UPDATE_SUCCESS) {
+            throw new RuntimeException("Server returned bad response: " + response.getType());
+        }
+    }
+
+    @Override
+    public void delete(String key) {
+        Response response = this.makeRequestUntilNotLocked(new DeleteRequest(key));
+
+        if (response.getType() != Response.Type.DELETE_SUCCESS) {
+            throw new RuntimeException("Server returned bad response: " + response.getType());
+        }
+    }
+
+    private Response makeRequest(Request request) {
+        try (
+                Socket connection = new Socket(this.host, this.port)
+        ) {
+            RequestWriter requestWriter = new RequestWriter(connection.getOutputStream());
+
+            requestWriter.writeRequest(request);
+
+            ResponseReader responseReader = new ResponseReader(connection.getInputStream());
+            return responseReader.readResponse();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Response makeRequestUntilNotLocked(Request request) {
+        long waitTime = 0;
+        Response response;
+        do {
+            response = this.makeRequest(request);
+
+            try {
+                Thread.sleep(waitTime);
+                if (waitTime <= MAX_WAIT_TIME) {
+                    waitTime += 10;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        } while (response.getType() == Response.Type.KEY_LOCKED);
+        return response;
+    }
+}
